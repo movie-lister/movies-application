f8b238f9c307519ffc95046eaa6e2b9e629e7566
MKovar <mkovar@mkovar.local>
2018-07-19 14:36:45 -0500

    form input, submission & js feat

--- src/index.js
+++ src/index.js
@@ -20,37 +20,66 @@ $(document).ready(function(){
 
 });
 
-getMovies().then((movies) => {
-// console.log(`id#${id} - ${title} - rating: ${rating}`)
-    $(".loading-gif").css("display", "none");
+const generateMovieList = () => {
+    getMovies().then((movies) => {
+    // console.log(`id#${id} - ${title} - rating: ${rating}`)
+        $(".loading-gif").css("display", "none");
 
 
-    let htmlBuilder = '<table id="movie-list">';
-    htmlBuilder += '<thead>';
-    htmlBuilder += '<tr>';
-    htmlBuilder+= '<th>Film</th>';
-    htmlBuilder += '<th>Rating</th>';
-    htmlBuilder += '<th>Options</th>';
-    htmlBuilder += '</tr>';
-    htmlBuilder += '</thead>';
+        let htmlBuilder = '<table id="movie-list">';
+        htmlBuilder += '<thead>';
+        htmlBuilder += '<tr>';
+        htmlBuilder+= '<th>Film</th>';
+        htmlBuilder += '<th>Rating</th>';
+        htmlBuilder += '<th>Options</th>';
+        htmlBuilder += '</tr>';
+        htmlBuilder += '</thead>';
 
-    movies.forEach(({title, rating, id}) => {
-        //populate the table with json response data
-        htmlBuilder += `<tr><td id="tdTitle">${title}</td>`;
-        htmlBuilder += `<td id="tdStars">${rating}</td>`;
-        htmlBuilder += `<td id="tdOptions">`
-            +
-            `<i class="far fa-trash-alt" data-id="${id}"></i></td>` +
-            `<i class="far fa-trash-alt" data-id="${id}"></i></td>` +
-            `</tr>`
+        movies.forEach(({title, rating, id}) => {
+            //populate the table with json response data
+            htmlBuilder += `<tr><td id="tdTitle">${title}</td>`;
+            htmlBuilder += `<td id="tdStars">${rating}</td>`;
+            htmlBuilder += `<td id="tdOptions">`
+                +
+                `<i class="far fa-trash-alt" data-id="${id}"></i>` +
+                `<i class="far fa-trash-alt" data-id="${id}"></i>` +
+                `</td></tr>`
 
+        });
+        htmlBuilder += '</table>';
+        $('#html-builder').html(htmlBuilder);
 
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
     });
-    htmlBuilder += '</table>';
-    $('#html-builder').html(htmlBuilder);
+};
+generateMovieList();
 
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
-    console.log(error);
+
+// submit form \\
+$('#submit-button').on('click', function(){
+    // e.preventDefault();
+   // include animate loading gif
+   let title = $('#movie-title').val();
+   let rating = $('#movie-rating').val();
+
+   let movie = {
+       title: title,
+       rating: rating
+   };
+
+   fetch('/api/movies', {
+       headers: {
+           "content-type": "application/json"
+       },
+       method: "POST",
+       body: JSON.stringify({title, rating})
+
+   }).then((response) => {
+       response.json();
+   }).then(() => {
+       generateMovieList();
+   });
+
 });
-
